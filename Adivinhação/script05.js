150ba45869bb5813fbb7e05b150ab3f164510a2d
Paulo3432 <pj248491@gmail.com>
2023-06-06 18:10:34 -0300

    Add files via upload

---
+++ Adivinhação/script05.js
@@ -0,0 +1,13 @@
+let result =  prompt("Adivinhe o número que estou pensando? Está entre 0 e 10")
+const randomNumber = Math.round(Math.random() * 10)
+ 
+let xAttempts = 1;
+
+while(Number(result) != randomNumber){
+
+    result = prompt("Erro, tente novamente:")
+    xAttempts++ 
+
+}
+
+alert(`Parabéns! Você advinhou o número em   ${xAttempts}  tentativas`)
